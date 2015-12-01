3a6ca7b7f56c5091c70bf12dd667baf2e6cda0ba
Jim Stanton <jim@jhstanton.com>
2015-12-01 10:24:33 -0600

    Added potential types for MIPs emulator

---
+++ examples/MIPs/Type.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE KindSignatures, GADTs #-}
+
+module MIPS.Type where
+
+import Language.Haskellator.Parse
+import Data.Word
+
+data MInstruction :: * where
+  R :: OP -> Address     -> Address -> Address   -> Shamt -> Func -> MInstruction
+  I :: OP -> Address     -> Address -> Immediate -> MInstruction
+  J :: OP -> JumpAddress -> MInstruction
+
+data OP :: * where -- all MIPS opcodes emulated
+  OP :: OP
+
+data Func :: * where -- all R-Type Functions emulated
+  Func :: Func
+
+type Address     = Word8
+type Shamt       = Word8
+type Immediate   = Word16
+type JumpAddress = Word32
+
+
+
