e6cec1696a35a03492dd28071150cd7b23d4c7d5
Jim Stanton <jim@jhstanton.com>
2015-12-02 01:44:56 -0600

    Added functions to construct instructions from parsed fields

--- examples/MIPs/Main.hs
+++ examples/MIPs/Main.hs
@@ -7,17 +7,18 @@ import MIPs.Types
 import Language.Haskellator.Parse
 
 import Control.Applicative
+import Control.Monad.State
 import Data.Word
 
 
--- TO get far enough to use these the first 6 bits will already be parsed (OP code field)
+-- To get far enough to use these the first 6 bits will already be parsed (OP code field)
 constructR :: Parser InstrWidth MInstruction 
 constructR = R <$> getField 5 <*> getField 5 <*> getField 5 <*> getField 5 <*> (getField 6 >>= return . toFunc)
 
-{-
-constructI :: Parser InstrWidth MInstruction
-constructI = I <$> getField 
 
-constructJ :: Parser InstrWidth M
-constructJ = J <$> 
--}
+constructI :: OP -> Parser InstrWidth MInstruction
+constructI op = I op <$> getField 5 <*> getField 5 <*> getField 16
+
+constructJ :: OP -> Parser InstrWidth MInstruction
+constructJ op = J op <$> getField 26
+
