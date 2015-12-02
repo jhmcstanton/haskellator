eb1f2e244ae1a0406e1a1e16d080856560a05fee
Jim Stanton <jim@jhstanton.com>
2015-12-02 01:58:56 -0600

    Added single constructor function

--- examples/MIPs/Main.hs
+++ examples/MIPs/Main.hs
@@ -11,6 +11,18 @@ import Control.Monad.State
 import Data.Word
 
 
+
+mkInstr :: Parser InstrWidth MInstruction
+mkInstr = do 
+  opBits <- getField 6
+  let op = toOP opBits
+  case op of
+    R_op -> constructR 
+    J_op -> constructJ op
+    JAL  -> constructJ op
+    _    -> constructI op
+    
+
 -- To get far enough to use these the first 6 bits will already be parsed (OP code field)
 constructR :: Parser InstrWidth MInstruction 
 constructR = R <$> getField 5 <*> getField 5 <*> getField 5 <*> getField 5 <*> (fmap toFunc $ getField 6)
