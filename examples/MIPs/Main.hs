f6431602b095b1342d7528c9fa36e559b2f8fade
Jim Stanton <jim@jhstanton.com>
2015-12-02 01:50:00 -0600

    Changed a bind to an fmap

--- examples/MIPs/Main.hs
+++ examples/MIPs/Main.hs
@@ -13,8 +13,7 @@ import Data.Word
 
 -- To get far enough to use these the first 6 bits will already be parsed (OP code field)
 constructR :: Parser InstrWidth MInstruction 
-constructR = R <$> getField 5 <*> getField 5 <*> getField 5 <*> getField 5 <*> (getField 6 >>= return . toFunc)
-
+constructR = R <$> getField 5 <*> getField 5 <*> getField 5 <*> getField 5 <*> (fmap toFunc $ getField 6)
 
 constructI :: OP -> Parser InstrWidth MInstruction
 constructI op = I op <$> getField 5 <*> getField 5 <*> getField 16
